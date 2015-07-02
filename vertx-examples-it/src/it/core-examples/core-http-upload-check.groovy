336a189082dd0ff2987c90a5e21fdbffaa0f6d52
Clement Escoffier <clement.escoffier@gmail.com>
2015-07-02 14:02:17 +0200

    Add upload and web socket tests

---
+++ vertx-examples-it/src/it/core-examples/core-http-upload-check.groovy
@@ -0,0 +1,7 @@
+helper.ensureSucceededInDeployingVerticle()
+helper.ensureClientSucceededInDeployingVerticle()
+helper.ensureTextNotContainedInErrorStream("Unhandled Exception")
+helper.ensureTextNotContainedInClientErrorStream("Unhandled Exception")
+
+helper.ensureTextInOutputStream("Uploaded to")
+helper.ensureTextInClientOutputStream("Response 200")
