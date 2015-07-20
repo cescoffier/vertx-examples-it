d6807d9c0d4e8d15b69c263e3791fbae4a0830a0
Clement Escoffier <clement.escoffier@gmail.com>
2015-07-20 19:17:10 +0200

    Fix groovy script

--- vertx-core-examples-it/src/it/core-examples/core-net-echo-check.groovy
+++ vertx-core-examples-it/src/it/core-examples/core-net-echo-check.groovy
@@ -7,7 +7,7 @@ helper.ensureTextNotContainedInClientErrorStream("Unhandled Exception")
 
 helper.ensureTextInOutputStream("Echo server is now listening")
 
-def String o = helper.client_output;
+def String o = helper.getClientOutput()
 assertThat(o).containsSequence(
         "Net client sending", "hello 0", "hello 1", "hello 2", "hello 4", "hello 6", "hello 9")
 
