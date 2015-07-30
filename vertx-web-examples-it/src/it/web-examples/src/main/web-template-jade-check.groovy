901af9e74fdcf4d66cc48dc2e36263c74777bbab
Clement Escoffier <clement.escoffier@gmail.com>
2015-07-30 16:17:54 +0200

    Automates examples using thyme leaf, handlebars and jade

---
+++ vertx-web-examples-it/src/it/web-examples/src/main/web-template-jade-check.groovy
@@ -0,0 +1,25 @@
+import groovyx.net.http.HTTPBuilder
+import org.apache.commons.io.IOUtils
+
+import static groovyx.net.http.ContentType.TEXT
+import static groovyx.net.http.Method.GET
+import static org.assertj.core.api.StrictAssertions.assertThat
+
+def http = new HTTPBuilder('http://localhost:8080')
+http.request(GET, TEXT) { req ->
+    response.success = { resp, reader ->
+        assert resp.status == 200
+        def data = IOUtils.toString(reader)
+        assertThat(data)
+                .contains("my jade template")
+                .contains("Hello Vert.x Web")
+    }
+
+    // called only for a 404 (not found) status code:
+    response.'404' = { resp ->
+        assert false
+    }
+}
+
+return true
+
