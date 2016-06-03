b0b2ad11323fb739bd32935f5254b3465b005e6e
Clement Escoffier <clement.escoffier@gmail.com>
2016-06-03 13:55:05 +0200

    Automate the eventbus SSL example

---
+++ vertx-core-examples-it/src/it/core-examples/core-eventbus-ssl-check.groovy
@@ -0,0 +1,6 @@
+helper.ensureSucceededInDeployingVerticle()
+helper.ensureClientSucceededInDeployingVerticle()
+helper.ensureTextNotContainedInErrorStream("Unhandled Exception")
+helper.ensureTextNotContainedInClientErrorStream("Unhandled Exception")
+
+helper.ensureTextInOutputStream("pong")
