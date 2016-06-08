6e5a26fad86a9993380ea6c043cd3b5fe52940a3
Clement Escoffier <clement.escoffier@gmail.com>
2016-06-08 21:03:20 +0200

    Remove size check in the mail examples

--- vertx-mail-examples-it/src/it/mail-examples/src/main/mail-headers-check.groovy
+++ vertx-mail-examples-it/src/it/mail-examples/src/main/mail-headers-check.groovy
@@ -4,6 +4,5 @@ helper.ensureTextInOutputStream("user1@example.com")
 helper.ensureTextInOutputStream("user2@example.com")
 helper.ensureTextInOutputStream("user3@example.com")
 helper.ensureTextInOutputStream("user4@example.com")
-helper.ensureTextInOutputStream("615 bytes")
 
 return true
--- vertx-mail-examples-it/src/it/mail-examples/src/main/mail-localhost-check.groovy
+++ vertx-mail-examples-it/src/it/mail-examples/src/main/mail-localhost-check.groovy
@@ -2,6 +2,5 @@
 helper.ensureSucceededInDeployingVerticle()
 helper.ensureTextInOutputStream("user@example.com")
 helper.ensureTextInOutputStream("other@example.com")
-helper.ensureTextInOutputStream("505 bytes")
 
 return true
