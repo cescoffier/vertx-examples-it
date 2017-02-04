b223bf70979eb45df44d338f05aeec03516733c7
Clement Escoffier <clement.escoffier@gmail.com>
2017-02-04 10:02:56 +0100

    removing jgroups examples

--- vertx-jgroups-examples-it/src/it/jgroups-examples/jgroups-check.groovy
+++
@@ -1,6 +0,0 @@
-helper.ensureSucceededInDeployingVerticle()
-helper.ensureClientSucceededInDeployingVerticle()
-helper.ensureTextNotContainedInErrorStream("Unhandled Exception")
-helper.ensureTextNotContainedInClientErrorStream("Unhandled Exception")
-
-return true
