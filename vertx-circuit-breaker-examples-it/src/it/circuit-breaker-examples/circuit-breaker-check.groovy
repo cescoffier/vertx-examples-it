7ea4df45e9dc0a08701dcbfd00b27bd6f9c56589
Clement Escoffier <clement.escoffier@gmail.com>
2017-02-13 18:02:54 +0100

    Automate circuit breaker examples

---
+++ vertx-circuit-breaker-examples-it/src/it/circuit-breaker-examples/circuit-breaker-check.groovy
@@ -0,0 +1,5 @@
+helper.ensureSucceededInDeployingVerticle()
+helper.ensureTextNotContainedInErrorStream("Unhandled Exception")
+helper.ensureTextNotContainedInClientErrorStream("Unhandled Exception")
+
+helper.ensureTextInClientOutputStream("Result: Bonjour")
