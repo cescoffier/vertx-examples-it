97631fc88e734c4e7a26736baaa050cf704ce411
Clement Escoffier <clement.escoffier@gmail.com>
2017-02-13 17:50:16 +0100

    Automate new JDBC streaming example

---
+++ vertx-jdbc-examples-it/src/it/jdbc-examples/jdbc-stream-check.groovy
@@ -0,0 +1,7 @@
+
+helper.ensureSucceededInDeployingVerticle()
+helper.ensureTextInOutputStream("[1,\"Hello\"]")
+helper.ensureTextInOutputStream("[2,\"Goodbye\"]")
+helper.ensureTextInOutputStream("[3,\"Cya Later\"]")
+
+return true
