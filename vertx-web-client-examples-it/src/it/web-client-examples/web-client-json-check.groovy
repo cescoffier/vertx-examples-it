b8b8d062e01081e388750826a218c1e2cc566e7b
Clement Escoffier <clement.escoffier@gmail.com>
2017-02-14 08:46:13 +0100

    Fix web client examples
    - some examples are not polyglot (POJO)
    - fix post check script

--- vertx-web-client-examples-it/src/it/web-client-examples/web-client-json-check.groovy
+++ vertx-web-client-examples-it/src/it/web-client-examples/web-client-json-check.groovy
@@ -3,5 +3,5 @@ helper.ensureClientSucceededInDeployingVerticle()
 helper.ensureTextNotContainedInErrorStream("Unhandled Exception")
 helper.ensureTextNotContainedInClientErrorStream("Unhandled Exception")
 
-helper.ensureTextInOutputStream("\"firstName\" : \"Dale\"")
-helper.ensureTextInOutputStream("\"lastName\" : \"Cooper\"")
+helper.ensureTextInOutputStream("\"Dale\"")
+helper.ensureTextInOutputStream("\"Cooper\"")
