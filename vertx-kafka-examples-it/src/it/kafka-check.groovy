955eb74e6f6494c01970902f9919d1c0be184a2c
Clement Escoffier <clement.escoffier@gmail.com>
2017-02-28 11:29:31 +0100

    Automate kafka examples

---
+++ vertx-kafka-examples-it/src/it/kafka-check.groovy
@@ -0,0 +1,41 @@
+import com.google.common.base.Predicate
+import org.fluentlenium.core.FluentPage
+import org.openqa.selenium.phantomjs.PhantomJSDriver
+import org.openqa.selenium.phantomjs.PhantomJSDriverService
+import org.openqa.selenium.remote.DesiredCapabilities
+
+import java.util.concurrent.TimeUnit
+
+DesiredCapabilities capabilities = new DesiredCapabilities();
+// Because script are loaded from https (CDN)
+capabilities.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS, ["--web-security=no", "--ignore-ssl-errors=yes"]);
+capabilities.setCapability("acceptSslCerts", true)
+PhantomJSDriverService service = PhantomJSDriverService.createDefaultService(capabilities)
+
+driver = new PhantomJSDriver(service, capabilities)
+helper.enqueueCloseable(driver)
+
+def page = new FluentPage(driver) {
+    public String getUrl() {
+        "http://localhost:8080"
+    }
+}
+
+page.go()
+
+def last
+page.await().pollingEvery(1, TimeUnit.SECONDS).atMost(15, TimeUnit.SECONDS)
+        .until((Predicate) {
+    last = page.$("#CPU-val").text
+    ! page.$("#CPU-val").isEmpty()  && ! last.isEmpty();
+})
+
+println "last: " + last
+
+page.await().pollingEvery(1, TimeUnit.SECONDS).atMost(5, TimeUnit.SECONDS)
+        .until((Predicate) {
+    println "value : " + page.$("#CPU-val").text + " / " + last
+    !page.$("#CPU-val").text.equals(last);
+})
+
+return true
