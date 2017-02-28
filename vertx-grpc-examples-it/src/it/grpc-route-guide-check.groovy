baa9e9ef61edf4e1916cd852ffcb7e422b5313e5
Clement Escoffier <clement.escoffier@gmail.com>
2017-02-28 13:54:34 +0100

    Automate gRPC examples

---
+++ vertx-grpc-examples-it/src/it/grpc-route-guide-check.groovy
@@ -0,0 +1,4 @@
+helper.ensureSucceededInDeployingVerticle()
+helper.ensureClientSucceededInDeployingVerticle()
+helper.ensureTextNotContainedInErrorStream("Unhandled Exception")
+helper.ensureTextNotContainedInClientErrorStream("Unhandled Exception")
